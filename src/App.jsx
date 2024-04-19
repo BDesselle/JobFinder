a6f240921a2ff69578b66289db1890e4d215f617
Brandon Desselle <desselle.brandon2@gmail.com>
2024-04-19 11:38:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={ <h1>My App</h1> } />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+    </Route>
+  )
 );
 
 const App = () => {
