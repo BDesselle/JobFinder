659a64610e54ccf8cc7400299ec8b6ccd027e7bf
Brandon Desselle <desselle.brandon2@gmail.com>
2024-04-19 12:58:32 -0500

    Update JobPage.jsx

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
