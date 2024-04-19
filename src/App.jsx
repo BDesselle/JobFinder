5555f43f84db976074c1d2400f00a72ae3f29825
Brandon Desselle <desselle.brandon2@gmail.com>
2024-04-19 11:19:10 -0500

    Syntax Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { 
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './components/ViewAllJobs'
 
-const App = () => {
-  return (
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-    </>
-  )
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={ <h1>My App</h1> } />)
+);
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
